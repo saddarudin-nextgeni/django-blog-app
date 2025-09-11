341823e6d2e8aa9d2f99abe95196a2e2d61d072c
Saddar U Din <saddarudin@nextgeni.com>
2025-09-11 14:33:19 +0500

    Week3-Day4: Defined Test cases for api app and untracked and remove all cached from all apps

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, CustomTokenView, LogoutView, MeView, HomeAPIView
+from .views import RegisterView, CustomTokenView, LogoutView, MeView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
